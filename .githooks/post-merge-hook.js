3c31a72f348c9de3db4841038a86192948853e4b
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2020-12-15 17:07:31 +0100

    Update post-merge-hook.js

--- .githooks/post-merge-hook.js
+++ .githooks/post-merge-hook.js
@@ -1,42 +1,93 @@
 #!/usr/bin/env node
 
+const figlet = require('figlet');
+const colors = require('color/safe');
 const util = require('util');
 const ch_proc = require('child_process');
 
 const $PWD = process.cwd()
 const JS_EOL = "\r\n"
 
-let output = [];
+let output = {};
 
-let exit_status = 0;
-
-console.log(JS_EOL + "\033[0;32m Now Running Post-Merge Hook - CoolCodes [ CodeSplinta ] Project \033[0m" + JSEOL + JS_EOL)
-console.lo(JS_EOL + "\033[0;32m Please Wait... \033[0m" + JS_EOL + JS_EOL + JSEOL)
-
+// console.log(JS_EOL + "\033[0;32m Now Running Post-Merge Hook - StitchApp [ FitCabinet | FitMeasure ] Project \033[0m" + JS_EOL + JS_EOL)
+console.log(colors.green.bold.underline(JS_EOL + 'Now Running Post-Merge Hook - CodeSplinta [ CoolCodes ] Project ' + JS_EOL + JS_EOL))
+// console.log(JS_EOL + "\033[0;32m Ready... \033[0m" + JS_EOL + JS_EOL + JS_EOL)
+console.log(colors.green.bold.underline(JS_EOL + 'Ready... ' + JS_EOL + JS_EOL + JS_EOL))
 
 let exec = util.promisify(ch_proc.exec)
+let cmdAsciiTextWriter = util.promisify(figlet);
 
-async function migration_refresh(){
-
-  try{
-      output = await exec('adonis migrate:refresh && adonis seed')
-      
+async function npm_install_refresh(){
+  
+  let exit_status = 0;
+  
+  try {
+      let _output = await exec('git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD');
+   
       if(output.stderr === ''){
-          console.log("\033[0;32m > Ended NodeJS Adonis Project Migration/Seed Routine - Success \033[0m" + JS_EOL + JS_EOL)
-      }else{
-           throw new Error(output.stdout.trim())
+          const isPackageJSONModified = (_output.stdout.trim().split('\n')).includes("package.json");
+        
+          if(isPackageJSONModified) {
+              await exec('pkg_skip_preinstall="yes" npm install')
+          }
       }
-  }catch({ message }){
-      console.log(output.stderr.trim() + JS_EOL + JS_EOL)
-      console.log("\033[0;31m > Ended NodeJS Adonis Project Migration/Seed Routine - Failure \033[0m" + JS_EOL + JS_EOL)
-      console.log("\033[0;31m >> Git Hook Error Message: \033[0m", message.split("\n"))
+  }catch(ex){ 
+       console.log(colors.red.bold.underline(ex.message + JS_EOL + JS_EOL));
     
-      exit_status = 1;
+       exit_status = 1;
   }
   
-  return exit_status
+  return exit_status;
 }
 
-migration_refresh().then(
-    process.exit.bind(process)
-)
+async function migration_refresh(){
+  
+  let exit_status = 0;
+  
+  try{
+    
+      await cmdAsciiTextWriter('StitchApp Project', { horizontalLayout: 'default', verticalLayout: 'default' });
+      output = await exec('adonis migration:refresh && adonis seed')
+      
+      if(output.stderr === ''){
+          // console.log("\033[0;32m > Ended NodeJS AdonisJS 4.1 Project Migration/Seed Routine - Success \033[0m" + JS_EOL + JS_EOL)
+          console.log(colors.green.bold.underline('> Ended NodeJS AdonisJS 4.1 Project Migration/Seed Routine - Success ' + JS_EOL + JS_EOL))
+      }else{
+           throw new Error((typeof output.stdout === 'string' && output.stdout.trim()) || (typeof output.stderr === 'string' && output.stderr.trim()) || '')
+      }
+  }catch({ message }){
+      console.log(message + JS_EOL + JS_EOL)
+      // console.log("\033[0;31m > Ended NodeJS AdonisJS 4.1 Project Migration/Seed Routine - Failure \033[0m" + JS_EOL + JS_EOL)
+    
+      if (!output.stdout) {
+        
+          console.log(colors.red.bold.underline('> Ended NodeJS AdonisJS 4.1 Project Migration/Seed Routine - Failure ' + JS_EOL + JS_EOL))
+          // console.log("\033[0;31m >> Git Hook Error Message: \033[0m", message.split("\n"))
+          console.log(colors.red.bold.underline('>> Git Hook Error Message: '), message.split('\n'));
+          
+          exit_status = 1;
+      }
+  }
+  
+  return exit_status;
+}
+
+migration_refresh().then(function(es){
+  if(es === 0){
+    return npm_install_refresh();
+  }
+  return es;
+}).then(function(_es) {
+  process.exit(_es);
+}).catch(function(error){
+  console.error(error);
+  process.exit(1)
+})
+
+/*
+process.on('unhandledRejection', function(error, promise){
+    console.error(error);
+    process.exit(1);
+});
+*/
